ab35c9610629a63d03447568a097becdd2ab0c61
dianikt <druizpena@iesesteveterradas.com>
2017-11-06 18:22:42 +0100

    final funciona

--- ajedrez.php
+++ ajedrez.php
@@ -26,6 +26,7 @@
 			}
 		</style>
 		<body>
+			<a href="destroy.php">Reiniciar Sesión</a>	
 			<br>
 			<form style="text-align:center;" method="POST" action="ajedrez.php">
 				<label>Escribe la posicion</label>
@@ -34,74 +35,78 @@
 			</form>
 			<br>
 			<table>
-				<?php 
+				<?php 				
 					if(!isset($_SESSION['rei_n']) != "" ){ //si no hay nada
 						$posicion = 'D0'; //doy primera posicion
 						$_SESSION['rei_n'] = $posicion;
 						$inicio = 1;
-
-					}else{
-						$rei = $_SESSION['rei_n'];
-						$letra = ord($rei);
-						$posArriba = $letra-1;
-						$posAbajo = $letra+1;
-						
-						if(($letra-1) == $posArriba || ($letra+1)==$posAbajo || $rei[1+1]==$rei[1+1]){
+					}else{	
+							$rei = $_SESSION['rei_n'];							
+							$vieja = ord($rei);							
 							$posicion = strtoupper($_POST["posicion"]);
-							$_SESSION['rei_n'] = $posicion;
-							$inicio = 0;
+							$_SESSION['rei_n'] = $posicion;	
+							$nueva = ord($posicion);
+							$inicio = 0;	
+							
+							//compruebo que no este en la misma posicion
+							if (($nueva == $vieja)&&($rei[1] == $posicion[1])) {
+								echo '<script language="javascript">alert("Estas en la misma posicion");</script>';
+							}
+							if(($nueva <= $vieja-2) || ($nueva >= $vieja+2)||( $posicion[1] > $rei[1]+1)||( $posicion[1] < $rei[1]-1)){
+								$posicion = $rei;
+								$_SESSION['rei_n'] = $rei;
+								echo '<script language="javascript">alert("NO se puede realizar este movimiento.");</script>';
+								
+							}
+							if(($posicion[1] > 8)||($nueva > 72)){ // para que no se salga del tablero
+								$_SESSION['rei_n'] = $rei;
+								echo '<script language="javascript">alert("NO se puede, debes volver hacia atrás.");</script>';
+							}
 						}
-						else echo '<script language="javascript">alert("No se puede hacer ese movimiento");</script>'; 
-					}
+						$fil =  7;
+						$col =  7;	
 
-					$fil =  7;
-				   	$col =  7;	
-
-				   	for ($x = 0; $x <= $fil+1; $x++) {						
-						echo "<tr>"; 		
-		    						
-		    			for ($y = 0; $y <= $col+1; $y++) {
-		    				if($y == 0){
-		    					$codigo = chr(64+$x);
-								echo "<td>$codigo</td>"; //linea de letras
-							}
-		    				if($x == 0){
-								echo "<td>$y</td>"; // primera linea de numeros
-							}
-							else {	
-								if(($x+$y) %2 == 0){	
-									if($inicio == 1) {							
-										echo "<td id='$codigo.$y' class='col'>";
-											if (($codigo =='D') && ($y == 0)) {
-												echo "<image id='imagen' src='rei.png'/>";
-											}											
-										echo"</td>"; //blanco
-									}
-									elseif($inicio == 0) {							
-										echo "<td id='$codigo.$y' class='col'>";
-											if (($codigo == $posicion[0]) && ($y == $posicion[1])) {
-												echo "<image style='background-color:white;' id='imagen' src='rei.png'/>";
-											}
-											
-										echo"</td>"; //blanco
-									}
+						for ($x = 0; $x <= $fil+1; $x++) {						
+							echo "<tr>"; 		
+										
+							for ($y = 0; $y <= $col+1; $y++) {
+								if($y == 0){
+									$codigo = chr(64+$x);
+									echo "<td>$codigo</td>"; //linea de letras
 								}
-								else{
-									echo "<td id='$codigo.$y'  class='fil'>";
-									if (($codigo == $posicion[0]) && ($y == $posicion[1])) {
-												echo "<image style='background-color:white;' id='imagen' src='rei.png'/>";
-											}
-									echo "</td>"; //negro
+								if($x == 0){
+									echo "<td>$y</td>"; // primera linea de numeros
 								}
-		    					
+								else {	
+									if(($x+$y) %2 == 0){	
+										if($inicio == 1) {							
+											echo "<td id='$codigo.$y' class='col'>";
+												if (($codigo =='D') && ($y == 0)) {
+													echo "<image id='imagen' src='rei.png'/>";
+												}											
+											echo"</td>"; //blanco
+										}
+										elseif($inicio == 0) {							
+											echo "<td id='$codigo.$y' class='col'>";
+												if (($codigo == $posicion[0]) && ($y == $posicion[1])) {
+													echo "<image style='background-color:white;' id='imagen' src='rei.png'/>";
+												}
+												
+											echo"</td>"; //blanco
+										}
+									}
+									else{
+										echo "<td id='$codigo.$y'  class='fil'>";
+										if (($codigo == $posicion[0]) && ($y == $posicion[1])) {
+													echo "<image style='background-color:white;' id='imagen' src='rei.png'/>";
+												}
+										echo "</td>"; //negro
+									}									
+								}														
 							}
-														
+							echo "</tr>";
 						}
-						echo "</tr>";
-					}				
-
-				?>
-			</table>
-			<a href="destroy.php">Cerrar Sesión</a>			
+					?>
+			</table>					
 		</body>
 </html>
--- inici.php
+++
@@ -1,5 +0,0 @@
-<FORM method="POS" action="ajedrez.php">
-	Filas: <INPUT type="number" name="fil" /><br />
-	Columnas: <INPUT type="number" name="col" /><br />
-	<INPUT type="submit" value="Envia" />
-</FORM>
